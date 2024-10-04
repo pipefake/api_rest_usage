564c6a974f174dceb72d94da471b7975a79376d1
Felipe Jimenez <jfjimenezsalazar@gmail.com>
2024-10-04 18:38:50 -0500

    project of coffe carrusel

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+
+import './App.css'
+import { ApiCoffeeCarruselBasic } from './components/ApiCoffeeCarruselBasic'
+
+function App() {
+
+
+  return (
+    <div>
+      <ApiCoffeeCarruselBasic />
+    </div>
+  )
+}
+
+export default App
